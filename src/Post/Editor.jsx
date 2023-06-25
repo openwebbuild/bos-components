ca3a7dadc76ba708d0e2befe08842241d81b1443
Robert Yan <46699230+think-in-universe@users.noreply.github.com>
2023-06-25 22:17:15 +0800

    feat: init project (#1)

---
+++ src/Post/Editor.jsx
@@ -0,0 +1,33 @@
+// MIT License: https://github.com/openwebbuild/bos-components/blob/main/LICENSE
+
+// config
+function getConfig(network) {
+  switch (network) {
+    case "mainnet":
+      return {
+        ownerId: "openwebbuild.near",
+        nodeUrl: "https://rpc.mainnet.near.org",
+      };
+    case "testnet":
+      return {
+        ownerId: "openwebbuild.testnet",
+        nodeUrl: "https://rpc.testnet.near.org",
+      };
+    default:
+      throw Error(`Unconfigured environment '${network}'.`);
+  }
+}
+const config = getConfig(context.networkId);
+
+const ComposeWrapper = styled.div`
+  border-top: 1px solid #eceef0;
+  height: 100vh;
+`;
+
+return (
+  <>
+    <ComposeWrapper>
+      <Widget src={`${config.ownerId}/widget/Post.Compose`} />
+    </ComposeWrapper>
+  </>
+);

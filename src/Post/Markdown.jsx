7230670f553d5692fa2336a6dffccaaf98cf5cc0
Robert Yan <mstr.raphael@gmail.com>
2023-10-21 09:12:18 +0800

    fix: meidum-like markdown viewer style

--- src/Post/Markdown.jsx
+++ src/Post/Markdown.jsx
@@ -18,19 +18,19 @@ function getConfig(network) {
 const config = getConfig(context.networkId);
 
 const Wrapper = styled.div`
-  font-family: Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif;
-  font-size: 16px;
+  font-family: source-serif-pro, Georgia, Cambria, "Times New Roman", Times,
+    serif;
+  font-size: 20px;
   font-weight: 400;
-  line-height: 24px;
-  color: #11181c;
+  line-height: 32px;
+  color: #242424;
   word-break: break-word;
 
   > * {
     margin-bottom: 12px;
   }
 
-  h1,
-  h2,
+  h1 h2,
   h3,
   h4,
   h5,
@@ -42,15 +42,29 @@ const Wrapper = styled.div`
   }
 
   h1 {
-    font-size: 21px;
+    font-family: sohne, "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 42px;
+    font-weight: 700;
   }
 
   h2 {
-    font-size: 18px;
+    font-family: sohne, "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 24px;
+    font-weight: 600;
+    margin-top: 1.95em;
+    margin-bottom: -0.28em;
   }
 
   p {
     white-space: pre-line;
+    letter-spacing: -0.003em;
+    margin-top: 2em;
+    margin-bottom: -0.46em;
+  }
+
+  h2 + p {
+    margin-top: 0.86em;
+    margin-bottom: -0.46em;
   }
 
   a {
